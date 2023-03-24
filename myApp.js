5e92fcc715f9658dbc1c5f6e9811750648b66ef9
RamonRMG <98474605+RamonRMG@users.noreply.github.com>
2023-03-24 23:16:42 +0000

    Finished 12th lesson from the Basic Node and Express

--- myApp.js
+++ myApp.js
@@ -1,9 +1,66 @@
+
 let express = require('express');
 let app = express();
-console.log('Hello World')
+const bodyParser = require('body-parser');
 
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+})
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+})
+
+app.use(express.static(__dirname + '/public'))
+app.use('/public', express.static(__dirname + '/public'))
+
+app.get("/json", (req, res) => {
+    res.json({
+      message: "Hello json"
+  });
+});
+
+if (process.env.VAR_NAME === "allCaps") {
+    response = "Hello World".toUpperCase();
+  } else {
+    response = "Hello World";
+}
+
+app.get("/now", (req, res, next) => {
+req.time = new Date().toString();
+next();
+},
+(req, res) =>{
+res.send({
+time: req.time
+  });
+ }
+);
+
+app.get("/:word/echo", (req, res) => {
+const { word } = req.params;
+res.json({
+echo: word
+ });
+});
+
+app.get("/name", (req, res) =>{
+res.json({ 
+name: req.query.first + " " + req.query.last
+ });
+});
+
+app.post("/name", (req, res) => {
+  res.json({
+    name: req.body.first + " " + req.body.last
+  })
+})
+
+module.exports = app
 
 
 
